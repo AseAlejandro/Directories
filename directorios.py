f9d0ea1e691cafe1e12886bf2b34e76f177b868f
AseAlejandro <asealejandro0@gmail.com>
2022-03-30 19:26:33 -0600

    Final

--- directorios.py
+++ directorios.py
@@ -3,3 +3,8 @@ import fleep
 from pathlib import Path
 import csv
 
+path = "C://Users//Alejandro//Desktop//Trabajo//Python//Directorios//Pictures"
+
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        print(os.path.join(subdir, file))
